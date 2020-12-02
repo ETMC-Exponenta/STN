ab0207e0fd0842387402156e23d8c435165aec73
Uran523 <psihologgg@mail.ru>
2020-12-02 11:06:40 +0300

    STN network

--- Numbers/Numbers/reshapeLayer.m
+++ Numbers/Numbers/reshapeLayer.m
@@ -5,7 +5,7 @@ classdef reshapeLayer < nnet.layer.Layer
     %   name-имя слоя, по умолчанию 'reshape'
     %
     % Тест слоя
-    %   layer = reshapeLayer("reshape4",[1,1,80])
+    %   layer = reshapeLayer("reshape",[1,1,80])
     %   checkLayer(layer, [2,2,20,128],'ObservationDimension', 4)
     
     properties (Constant)
@@ -22,10 +22,7 @@ classdef reshapeLayer < nnet.layer.Layer
         end
         
         function Z = predict(layer, varargin)
-            S=layer.SizeOut;
-            for i=1:1:size(varargin{1},4)
-                Z(1:S(1),1:S(2),1:S(3),i)=reshape(varargin{1}(:,:,:,i),S(1),S(2),S(3));
-            end
+            Z=reshape(varargin{1},layer.SizeOut(1),layer.SizeOut(2),layer.SizeOut(3),[]);
         end
     end
 end
--- Window/reshapeLayer.m
+++ Window/reshapeLayer.m
@@ -5,7 +5,7 @@ classdef reshapeLayer < nnet.layer.Layer
     %   name-имя слоя, по умолчанию 'reshape'
     %
     % Тест слоя
-    %   layer = reshapeLayer("reshape4",[1,1,80])
+    %   layer = reshapeLayer("reshape",[1,1,80])
     %   checkLayer(layer, [2,2,20,128],'ObservationDimension', 4)
     
     properties (Constant)
@@ -16,16 +16,13 @@ classdef reshapeLayer < nnet.layer.Layer
     end
     
     methods
-        function layer = reshapeLayer(name, SizeOut)%Объявление
+        function layer = reshapeLayer(name, SizeOut)
             layer.Name = name;
             layer.SizeOut=SizeOut;
         end
         
-        function Z = predict(layer, varargin)% Во время обучения
-            S=layer.SizeOut;
-            for i=1:1:size(varargin{1},4)
-                Z(1:S(1),1:S(2),1:S(3),i)=reshape(varargin{1}(:,:,:,i),S(1),S(2),S(3));
-            end
+        function Z = predict(layer, varargin)
+            Z=reshape(varargin{1},layer.SizeOut(1),layer.SizeOut(2),layer.SizeOut(3),[]);
         end
     end
 end
